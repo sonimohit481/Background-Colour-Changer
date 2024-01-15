03fefa53801248aa5c2bd6a6c224d62186facf21
Mohit Soni <sonimohit481@gmail.com>
2024-01-15 18:54:24 +0530

    Basic project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 function App() {
+  const [colour, setColour] = useState("Black");
+
+  const colorPalette = [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Purple",
+    "Pink",
+    "Turquoise",
+    "Lime",
+    "Indigo",
+    "Crimson",
+    "Teal",
+    "Salmon",
+    "SlateGray",
+    "Gold",
+  ];
+
   return (
     <>
-      <h1>HELLO SONI</h1>
+      <div className="w-full h-screen" style={{ backgroundColor: colour }}>
+        <div className="absolute bottom-10 p-3 inset-x-5 flex flex-wrap gap-1 justify-between">
+          {colorPalette.map((color) => (
+            <div
+              key={color}
+              className="px-4 py-3 rounded-lg text-white font-bold drop-shadow-xl"
+              onClick={() => setColour(color)}
+              style={{ backgroundColor: color }}
+            >
+              {color}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
