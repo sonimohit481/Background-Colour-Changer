6245093a171c2a58b429142fb0f56ad66387e659
Mohit Soni <sonimohit481@gmail.com>
2024-12-31 12:49:05 +0530

    showing basic first

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./compnents";
 import Version1 from "./screens/Version1";
 
 const App = () => {
-  const [isBasic, setIsBasic] = useState(false);
+  const [isBasic, setIsBasic] = useState(true);
   const [currentColor, setCurrentColor] = useState<string>("#fafafa");
   return (
     <div className="w-full h-screen" style={{ backgroundColor: currentColor }}>
